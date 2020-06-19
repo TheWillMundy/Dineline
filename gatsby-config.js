57ea5f9c5362eca0269110ee2232da072246c8e2
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-06-18 21:39:58 -0500

    Added ability to use Square's Rest API with Gatsby; configured a call to the Catalog API

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -5,6 +11,77 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: "gatsby-source-apiserver",
+      options: {
+        // Type prefix of entities from server
+        typePrefix: "square__",
+
+        // The url, this should be the endpoint you are attempting to pull data from
+        url: `${process.env.GATSBY_SQUARE_API_URL}`,
+
+        method: "get",
+
+        headers: {
+          "authorization": `bearer ${process.env.GATSBY_SQUARE_API_TOKEN}`,
+          "Square-Version": "2020-05-28",
+          "Content-Type": "application/json",
+        },
+
+        // Request body
+        data: null,
+
+        // Name of the data to be downloaded.  Will show in graphQL or be saved to a file
+        // using this name. i.e. posts.json
+        name: `square_catalog`,
+
+        // Nested level of entities in response object, example: `data.posts`
+        entityLevel: ``,
+
+        // Request parameters
+        // Only available from version 2.1.0
+        params: {
+          // per_page: 1,
+          types: "MODIFIER_LIST,ITEM,ITEM_VARIATION"
+        },
+
+        // Optional payload key name if your api returns your payload in a different key
+        // Default will use the full response from the http request of the url
+        payloadKey: `objects`,
+
+        // Optionally save the JSON data to a file locally
+        // Default is false
+        localSave: false,
+
+        //  Required folder path where the data should be saved if using localSave option
+        //  This folder must already exist
+        // path: `${__dirname}/src/data/auth/`,
+
+        // Optionally include some output when building
+        // Default is false
+        verboseOutput: true, // For debugging purposes
+
+        // Optionally skip creating nodes in graphQL.  Use this if you only want
+        // The data to be saved locally
+        // Default is false
+        skipCreateNode: false, // skip import to graphQL, only use if localSave is all you want
+
+        // Optionally re-source data when it changes and
+        // `gatsby develop` is running.
+        // Requires `ENABLE_GATSBY_REFRESH_ENDPOINT=true`.
+        // See https://www.gatsbyjs.org/docs/environment-variables/#reserved-environment-variables
+        // Default is false
+        enableDevRefresh: true,
+
+        // Optionally override key used to re-source data
+        // when `gatsby develop` is running.
+        // Requires `enableDevRefresh: true`.
+        // See setting directly above this one.
+        // See also https://github.com/gatsbyjs/gatsby/issues/14653
+        // Default is `id`
+        refreshId: `id`,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
